3add9542b7fa8d96e6c042a41727aa154a5931a1
mariuszkalinskismt <mariusz.kalinski@smtsoftware.com>
2017-07-11 14:47:37 +0200

    before merge streams

--- app/components/player/player.js
+++ app/components/player/player.js
@@ -8,63 +8,44 @@ export default new Component({
   `,
   styles,
   controller: (scope) => {
-    const gridSize = { x: 600, y: 600};
+    const gridSize = {
+      x: 600,
+      y: 600
+    };
     const playerSize = 50;
-    const gridBoxes = {x: gridSize.x / playerSize, y: gridSize.y / playerSize};
-    let playerPosition = {x: 0, y: 0};
+    const gridBoxes = {
+      x: gridSize.x / playerSize,
+      y: gridSize.y / playerSize
+    };
+    let playerPosition = {
+      x: 0,
+      y: 0
+    };
 
     const target = scope.target;
     target.style.width = playerSize;
     target.style.height = playerSize;
     const range = 1;
-    const goRight = () => {
-      if (playerPosition.x < gridBoxes.x - 1) {
-        playerPosition.x += range;
-        target.style.left = playerPosition.x * playerSize;
-      }
+
+    const increase = (property, direction) => {
+      playerPosition[property] += range;
+      target.style[direction] = playerPosition[property] * playerSize;
     }
-    const goLeft = () => {
-      if (playerPosition.x > 0) {
-        playerPosition.x -= range;
-        target.style.left = playerPosition.x * playerSize;
-      }
+    const decrease = (property, direction) => {
+      playerPosition[property] -= range;
+      target.style[direction] = playerPosition[property] * playerSize;
     }
-    const goUp = () => {
-      if (playerPosition.y > 0) {
-        playerPosition.y -= range;
-        target.style.top = playerPosition.y * playerSize;
-      }
-    }
-    const goDown = () => {
-      if (playerPosition.y < gridBoxes.y - 1) {
-        playerPosition.y += range;
-        target.style.top = playerPosition.y * playerSize;
-      }
-    }
-    // const source = Rx.Observable.fromEvent(document, 'keydown');
-    // const subscribe = source.subscribe(event => {
-    //   switch (event.key) {
-    //     case 'ArrowUp':
-    //       goUp();
-    //       break;
-    //     case 'ArrowDown':
-    //       goDown();
-    //       break;
-    //     case 'ArrowLeft':
-    //       goLeft();
-    //       break;
-    //     case 'ArrowRight':
-    //       goRight();
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // });
     const left = Rx.Observable.fromEvent(document, 'keydown')
-      .filter(event => event.key === 'ArrowLeft')
-      .subscribe(x => goLeft());
+      .filter(event => event.key === 'ArrowLeft' && playerPosition.x > 0)
+    // .subscribe(x => decrease('x', 'left'));
     const right = Rx.Observable.fromEvent(document, 'keydown')
-      .filter(event => event.key === 'ArrowRight')
-      .subscribe(x => goRight());      
+      .filter(event => event.key === 'ArrowRight' && playerPosition.x < gridBoxes.x - 1)
+    // .subscribe(x => increase('x', 'left'));
+    const top = Rx.Observable.fromEvent(document, 'keydown')
+      .filter(event => event.key === 'ArrowUp' && playerPosition.y > 0)
+    // .subscribe(x => decrease('y', 'top'));
+    const bottom = Rx.Observable.fromEvent(document, 'keydown')
+      .filter(event => event.key === 'ArrowDown' && playerPosition.y < gridBoxes.y - 1)
+    // .subscribe(x => increase('y', 'top'));
   }
 });
